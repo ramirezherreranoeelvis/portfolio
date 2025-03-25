9540ac443685dd0e5a070a63004974e3026c7c86
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-03-25 03:29:25 -0500

    feat: add responsive tittle :S

--- src/app/components/templates/about-me-template/about-me-template.component.ts
+++ src/app/components/templates/about-me-template/about-me-template.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'about-me-template',
-  imports: [],
-  templateUrl: './about-me-template.component.html',
-  styleUrl: './about-me-template.component.scss'
+      selector: 'about-me-template',
+      templateUrl: './about-me-template.component.html',
+      styleUrl: './about-me-template.component.scss',
 })
-export class AboutMeTemplateComponent {
-
-}
+export class AboutMeTemplateComponent {}
