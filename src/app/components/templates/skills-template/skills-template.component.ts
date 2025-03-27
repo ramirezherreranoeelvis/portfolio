492467fdec36b23f4835e06d3dc9e35b01b9f850
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-03-27 02:50:34 -0500

    refactor: move images routes in ts skillscomponent

--- src/app/components/templates/skills-template/skills-template.component.ts
+++ src/app/components/templates/skills-template/skills-template.component.ts
@@ -1,11 +1,72 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'skills-template',
-  imports: [],
-  templateUrl: './skills-template.component.html',
-  styleUrl: './skills-template.component.scss'
+      selector: 'skills-template',
+      imports: [],
+      templateUrl: './skills-template.component.html',
+      styleUrl: './skills-template.component.scss',
 })
 export class SkillsTemplateComponent {
-
+      routesImages: IRouteImages[] = [
+            {
+                  src: 'images/skills/angular.ico',
+                  alt: 'angular',
+            },
+            {
+                  src: 'images/skills/spring_boot.png',
+                  alt: 'spring_boot',
+            },
+            {
+                  src: 'images/skills/react.png',
+                  alt: 'react',
+            },
+            {
+                  src: 'images/skills/node.png',
+                  alt: 'node',
+            },
+            {
+                  src: 'images/skills/MySQL.png',
+                  alt: 'mysql',
+            },
+            {
+                  src: 'images/skills/css.png',
+                  alt: 'css',
+            },
+            {
+                  src: 'images/skills/html.png',
+                  alt: 'html',
+            },
+            {
+                  src: 'images/skills/java.png',
+                  alt: 'java',
+            },
+            {
+                  src: 'images/skills/mongodb.png',
+                  alt: 'mongodb',
+            },
+            {
+                  src: 'images/skills/nest.png',
+                  alt: 'nest',
+            },
+            {
+                  src: 'images/skills/js.png',
+                  alt: 'javascript',
+            },
+            {
+                  src: 'images/skills/sass.png',
+                  alt: 'sass',
+            },
+            {
+                  src: 'images/skills/ts.png',
+                  alt: 'typescript',
+            },
+            {
+                  src: 'images/skills/SQLServer.png',
+                  alt: 'SQLServer',
+            },
+      ];
+}
+interface IRouteImages {
+      src: string;
+      alt: string;
 }
