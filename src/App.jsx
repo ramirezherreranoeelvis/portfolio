bbb33738c78504c042f7fd3837eb25f8877b544c
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-07-28 06:56:10 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const Content = () => {
                     )}
                 </InView>
             </div>
-            {/* <StarsCanvas /> */}
+            <StarsCanvas />
         </>
     );
 };
